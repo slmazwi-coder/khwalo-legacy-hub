cb512af5cbb7b5507e769de1a7e17fcb1d6036f9
Someleze Lucky mazwi <slmazwi@gmail.com>
2026-04-04 17:01:16 +0200

    Add Updates section wrapper combining testimonials and funerals

---
+++ src/components/UpdatesSection.tsx
@@ -0,0 +1,11 @@
+import TestimonialsSection from "@/components/TestimonialsSection";
+import UpcomingFunerals from "@/components/UpcomingFunerals";
+
+const UpdatesSection = () => (
+  <section id="updates" className="bg-background">
+    <TestimonialsSection />
+    <UpcomingFunerals />
+  </section>
+);
+
+export default UpdatesSection;
